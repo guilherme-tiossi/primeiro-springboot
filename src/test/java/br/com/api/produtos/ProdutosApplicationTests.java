8405d1a991a5124515b4584e73ade91cf1ee0d5b
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-05-16 20:40:34 -0300

    Alteração de projeto

--- src/test/java/br/com/api/produtos/ProdutosApplicationTests.java
+++ src/test/java/br/com/api/produtos/ProdutosApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package br.com.api.produtos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ProdutosApplicationTests {
 
 	@Test
 	void contextLoads() {
