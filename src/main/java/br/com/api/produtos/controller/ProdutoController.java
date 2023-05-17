4baf07d92316b4eab4cdaf82fab644453191a429
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-05-16 22:48:04 -0300

    Primeiros passos com o Spring!

--- src/main/java/br/com/api/produtos/controller/ProdutoController.java
+++ src/main/java/br/com/api/produtos/controller/ProdutoController.java
@@ -1,11 +1,36 @@
 package br.com.api.produtos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.api.produtos.model.ProdutoModel;
+import br.com.api.produtos.services.ProdutoService;
 
 @RestController
 public class ProdutoController {
     
+    @Autowired
+    private ProdutoService ps;
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterarProduto(@RequestBody ProdutoModel pm){
+        return ps.cadastrarAlterarProduto(pm, "alt");
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrarProduto(@RequestBody ProdutoModel pm){
+        return ps.cadastrarAlterarProduto(pm, "cad");
+    }
+    
+    @GetMapping("/listar")
+    public Iterable<ProdutoModel> listar(){
+        return ps.listarProdutos();
+    }
+
     @GetMapping("/")
     public String rota(){
         return "API de produtos funcionando!";
--- src/main/java/br/com/api/produtos/services/ProdutoService.java
+++ src/main/java/br/com/api/produtos/services/ProdutoService.java
@@ -1,5 +1,35 @@
 package br.com.api.produtos.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import br.com.api.produtos.model.ProdutoModel;
+import br.com.api.produtos.model.RespostaModel;
+import br.com.api.produtos.repository.ProdutoRepository;
+
+@Service
 public class ProdutoService {
-    
+    @Autowired
+    private ProdutoRepository pr;
+
+    @Autowired
+    private RespostaModel rm;
+
+    public Iterable<ProdutoModel> listarProdutos(){
+        return pr.findAll();
+    }
+
+    public ResponseEntity<?> cadastrarAlterarProduto(ProdutoModel pm, String acao){
+        if(pm.getNome().equals("") || pm.getPreco() < 0){
+            rm.setMensagem("Ambos os campos de nome e de produto devem estar preenchidos");
+            return new ResponseEntity<RespostaModel>(rm, HttpStatus.BAD_REQUEST);
+        } else {
+            if(acao.equals("cad")){
+                return new ResponseEntity<ProdutoModel>(pr.save(pm), HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<ProdutoModel>(pr.save(pm), HttpStatus.OK);
+            }
+        }
+    }
 }
