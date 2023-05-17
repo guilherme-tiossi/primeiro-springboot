61edb93736d70140149062656568a1b77d942df2
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-05-16 21:35:53 -0300

    Configurações iniciais

---
+++ src/main/java/br/com/api/produtos/controller/ProdutoController.java
@@ -0,0 +1,13 @@
+package br.com.api.produtos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProdutoController {
+    
+    @GetMapping("/")
+    public String rota(){
+        return "API de produtos funcionando!";
+    }
+}
